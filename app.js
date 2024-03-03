c51bc728dba699995c391502c7ebbc66773df0f4
chalani <chalasaumya@gmail.com>
2024-03-03 16:58:41 +0530

    initial commit

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-const fs = require('fs');
-fs.writeFile('file.txt', 'Hello World!', function (err) {
-    if (err) throw err;
-    console.log('File saved!');
-});
+const http = require('http');
+http.createServer(function (req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write('Hello World!');
+    res.end();
+}).listen(8080);
