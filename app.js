6900af6a37f60b0df41c0d1e908c141f6fb11882
chalani <chalasaumya@gmail.com>
2024-03-03 17:00:03 +0530

    initial commit

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
-const http = require('http');
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write('Hello World!');
-    res.end();
-}).listen(8080);
+const https = require('https');
+https.get('https://jsonplaceholder.typicode.com/posts/1', (resp) => {
+    let data = '';
+    resp.on('data', (chunk) => {
+        data += chunk;
+    });
+    resp.on('end', () => {
+        console.log(JSON.parse(data));
+    });
+}).on('error', (err) => {
+    console.log("Error: " + err.message);
+});
