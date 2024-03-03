dfb0f5d0c8fc88ea52405bbba135e7b36d89606e
chalani <chalasaumya@gmail.com>
2024-03-03 17:03:52 +0530

    initial commit

--- app.js
+++ app.js
@@ -1,12 +1,2 @@
-const https = require('https');
-https.get('https://jsonplaceholder.typicode.com/posts/1', (resp) => {
-    let data = '';
-    resp.on('data', (chunk) => {
-        data += chunk;
-    });
-    resp.on('end', () => {
-        console.log(JSON.parse(data));
-    });
-}).on('error', (err) => {
-    console.log("Error: " + err.message);
-});
+const myModule = require('./my-module.js');
+console.log(myModule.myFunction());
